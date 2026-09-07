7f2252ac1c1cd09779fb0bdeb61afccbaf4213d2
chekad2525 <chekad2525@proton.me>
2026-09-07 12:52:12 +0330

    Create migration.sql

---
+++ packages/db/prisma/migrations/20260907000001_repair_internal_api_nonce/migration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "InternalApiNonce" Locksmith:\/\/ (
+  "nonce" TEXT NOT NULL,
+  "expiresAt" TIMESTAMP(3) NOT NULL,
+  CONSTRAINT "InternalApiNonce_pkey" PRIMARY KEY ("nonce")
+);
+
+CREATE INDEX IF NOT EXISTS "InternalApiNonce_expiresAt_idx"
+  ON "InternalApiNonce"("expiresAt");
